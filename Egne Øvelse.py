6fc48a7e4dc156d646c1875020b52d2a32442854
Ronni Jensen <rje040786@hotmail.com>
2026-01-08 10:22:31 +0100

    ChatGPT Hjælp med kode opsætning

--- Egne Øvelse.py
+++ Egne Øvelse.py
@@ -1,17 +1,20 @@
-from selectors import SelectSelector
-
 Besked = "Hej verden"
 print(Besked + "!")
 
-navn = input("Hvad Hedder du: ")
+navn = input("Hvad hedder du: ")
 print("Hej " + navn + "!")
-
 
 alder = int(input("Hvor gammel er du: "))
 
 if alder >= 18:
     print("Du er over 18")
-else :
+else:
     print("Du er under 18 år")
 
+tal = 0
+while tal < alder:
+    tal = tal + 1
+    print(tal)
 
+for tal in range(1, alder + 1):
+    print(tal)
