f0670c9be28c8c249ac0873e97191fb3f16a871f
Ronni Jensen <rje040786@hotmail.com>
2026-01-09 10:28:33 +0100

    Eksame: Opgave 1, ny version

--- Eksame opgave 1.py
+++ Eksame opgave 1.py
@@ -1,13 +1,18 @@
-
-from email.quoprimime import body_check, header_check
-from idlelib.multicall import r
-
 import requests
 
-response = requests.get(
-'http://20.91.198.208', headers={'Authorization': 'access_token df2e6ced'})
+IP = "20.91.198.208"
+PORT = 8081
+TOKEN = "df2e6ced"
 
-var = r.status_code
-var = response.status_code
-var = body_check()
-var = header_check()
+url = f"http://{IP}:{PORT}"
+
+response = requests.get(
+    url,
+    params={"token": TOKEN}
+)
+
+print("=== RESPONSE HEADERS ===")
+print(response.headers)
+
+print("\n=== RESPONSE BODY ===")
+print(response.text)
